fce93bdcf8c79c4a940163e88f8b36deadef60a1
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-05-04 18:52:17 +1000

    Update brute_force_text.py
    + Added .py documentation with official reStructuredText:
    https://realpython.com/documenting-python-code/

--- py/brute_force_text.py
+++ py/brute_force_text.py
@@ -1,15 +1,26 @@
 import math as Math
 
-# The maximum number of characters
+"""The maximum number of characters
+"""
 MAX_LENGTH = 3
 
-# Characters
+"""Characters to be used for brute force string generation
+"""
 CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !#$%\&()*+,-./:;<=>?@[\]^_{|}~"
 
+"""List of all possible strings
+"""
 text = []
 
 
 def decimalToText(num):
+    """Converts a decimal number to a string using the CHARACTERS list
+
+    :param num: Decimal number to convert
+    :type num: int
+    :returns: String representation of the decimal number
+    :rtype: str
+    """
     str = ""
     while (num != 0):
         str += CHARACTERS[(num - 1) % len(CHARACTERS)]
